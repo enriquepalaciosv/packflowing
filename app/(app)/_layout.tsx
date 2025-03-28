c896eb5f72cfa10ed11e682debc31c85dd1ae537
Lisandro Salvareschi <salvareschilisandro@gmail.com>
2025-03-28 01:56:44 +0000

    change tabs for stack in layout (app)

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,68 +1,23 @@
-import { Platform, Text } from "react-native";
-import { Redirect } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { Text } from "react-native";
 import { useSession } from "../../contexts/authentication";
-import { Tabs } from "expo-router";
-import { Feather } from "@expo/vector-icons";
 
 export default function AppLayout() {
   const { session, isLoading } = useSession();
 
-  // You can keep the splash screen open, or render a loading screen like we do here.
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
-
-  // Only require authentication within the (app) group's layout as users
-  // need to be able to access the (auth) group and sign in again.
-  if (!session) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
-    return <Redirect href="/sign-in" />;
-  }
-
-  // This layout can be deferred because it's not the root layout.
+  if (isLoading) return <Text>Loading...</Text>;
+  if (!session) return <Redirect href="/sign-in" />;
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: Platform.select({
-          ios: {
-            position: "absolute",
-            height: "auto",
-            paddingHorizontal: 75,
-          },
-          default: {
-            height: "auto",
-          },
-        }),
-        tabBarItemStyle: {
-          width: 20,
-          marginHorizontal: 20,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Paquetes",
-          tabBarIcon: ({ color }) => (
-            <Feather size={28} name="package" color={color} />
-          ),
-          tabBarIconStyle: { marginBottom: 5 },
-          tabBarLabelStyle: { fontSize: 12 },
-        }}
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="(tabs)"
+        options={{ headerShown: false, title: "Inicio" }}
       />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Perfil",
-          tabBarIcon: ({ color }) => (
-            <Feather size={28} name="user" color={color} />
-          ),
-          tabBarIconStyle: { marginBottom: 5 },
-          tabBarLabelStyle: { fontSize: 12 },
-        }}
+
+      <Stack.Screen
+        name="listAllPackages"
+        options={{ headerShown: true, title: "Detalles" }}
       />
-    </Tabs>
+    </Stack>
   );
 }
