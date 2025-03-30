c70522a114ce10cadbbaa1d117f3affcdb34a4fe
Lisandro Salvareschi <salvareschilisandro@gmail.com>
2025-03-30 03:27:31 +0000

    create refreshAnimation in section list

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,21 +1,21 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
+  Animated,
+  RefreshControl,
   SafeAreaView,
   SectionList,
   StyleSheet,
-  RefreshControl,
-  View,
   TouchableOpacity,
+  View,
 } from "react-native";
 import { Text } from "react-native-paper";
-import { useState, useCallback, useEffect } from "react";
 import HeaderPackages from "../../components/HeaderPackages";
 import PackageItem from "../../components/PackageItem";
-import useGetPackages from "../../hooks/useGetPackages";
-import useGetUserData from "../../hooks/useGetUserData";
 import { useSession } from "../../contexts/authentication";
+import useGetPackages from "../../hooks/useGetPackages";
+import RefreshAnimation from "../../components/RefreshAnimation";
 
 export default function Index() {
-  // const { userData } = useGetUserData();
   const { packages, isLoading, reloadPackages } = useGetPackages(); // Añadido `reloadPackages`
   const [refreshing, setRefreshing] = useState(false);
   const { session } = useSession();
@@ -37,54 +37,60 @@ export default function Index() {
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <SectionList
-        keyExtractor={(_, index) => index.toString()}
-        sections={packages.map((section) => ({
-          ...section,
-          total: section.data.length, // Número total de paquetes
-          totalData: section.data, // Listado completo de paquetes
-          data: section.data.slice(0, 5), // Solo los cinco más recientes
-        }))}
-        ListHeaderComponent={
-          <HeaderPackages name={session.name} lockerCode={session.lockerCode} />
-        }
-        ListEmptyComponent={() => <></>}
-        renderSectionHeader={({ section }) =>
-          section.total ? (
-            <View
-              style={{
-                position: "relative",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}
-            >
-              <Text variant="titleMedium" style={{ marginVertical: 10 }}>
-                {`${section.title} (${section.total})`}
-              </Text>
-              {section.total > 5 ? (
-                <TouchableOpacity style={{}}>
-                  <Text>Ver todos</Text>
-                </TouchableOpacity>
-              ) : null}
-            </View>
-          ) : null
-        }
-        stickySectionHeadersEnabled={false}
-        stickyHeaderHiddenOnScroll={false}
-        renderItem={({ item, section }) => (
-          <PackageItem
-            section={section.title}
-            via={item.via}
-            title={item.idRastreo}
-            name={session.name + " " + session.lastName}
-          />
-        )}
-        contentContainerStyle={styles.sectionList}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      />
+      <RefreshAnimation>
+        <SectionList
+          keyExtractor={(_, index) => index.toString()}
+          sections={packages.map((section) => ({
+            ...section,
+            total: section.data.length, // Número total de paquetes
+            totalData: section.data, // Listado completo de paquetes
+            data: section.data.slice(0, 5), // Solo los cinco más recientes
+          }))}
+          ListHeaderComponent={
+            <HeaderPackages
+              name={session.name}
+              lockerCode={session.lockerCode}
+            />
+          }
+          ListEmptyComponent={() => <></>}
+          renderSectionHeader={({ section }) =>
+            section.total ? (
+              <View
+                style={{
+                  position: "relative",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text variant="titleMedium" style={{ marginVertical: 10 }}>
+                  {`${section.title} (${section.total})`}
+                </Text>
+                {section.total > 5 ? (
+                  <TouchableOpacity style={{}}>
+                    <Text>Ver todos</Text>
+                  </TouchableOpacity>
+                ) : null}
+              </View>
+            ) : null
+          }
+          stickySectionHeadersEnabled={false}
+          stickyHeaderHiddenOnScroll={false}
+          renderItem={({ item, section }) => (
+            <PackageItem
+              section={section.title}
+              via={item.via}
+              title={item.idRastreo}
+              name={session.name + " " + session.lastName}
+            />
+          )}
+          contentContainerStyle={styles.sectionList}
+          refreshing={isLoading}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        />
+      </RefreshAnimation>
     </SafeAreaView>
   );
 }
