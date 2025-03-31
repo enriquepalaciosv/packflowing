68b726e83a6c877e47fa7d77d587f959f1276f6e
Lisandro <salvareschilisandro0@gmail.com>
2025-03-31 22:49:29 +0000

    change method for group by date

--- utils/groupPackagesByDate.ts
+++ utils/groupPackagesByDate.ts
@@ -3,7 +3,7 @@ import { Paquete } from "../interfaces/packages";
 // Función para ordenar y agrupar los paquetes por la fecha del último rastreo
 export default function groupAndSortPackagesByRastreo(
   paquetes: Paquete[]
-): Record<string, Paquete[]> {
+): { title: string, data: Paquete[] }[] {
   // Ordenar los paquetes por la fecha y hora del último rastreo
   const paquetesOrdenados = paquetes.sort((a, b) => {
     const lastRastreoA = a.rastreo[a.rastreo.length - 1];
@@ -16,19 +16,22 @@ export default function groupAndSortPackagesByRastreo(
   });
 
   // Agrupar los paquetes por fecha
-  const agrupadoPorFecha: Record<string, Paquete[]> = {};
+  const agrupadoPorFecha: { title: string, data: Paquete[] }[] = [];
 
   paquetesOrdenados.forEach((paquete) => {
     const lastRastreo = paquete.rastreo[paquete.rastreo.length - 1];
     const fecha = lastRastreo.fecha;
-
+    let index = agrupadoPorFecha.findIndex(x => x.title === fecha);
     // Si no existe una entrada para esa fecha, la creamos
-    if (!agrupadoPorFecha[fecha]) {
-      agrupadoPorFecha[fecha] = [];
+    if (index === -1) {
+      agrupadoPorFecha.push({
+        title: fecha,
+        data: [paquete]
+      });
+    } else {
+      // Añadir el paquete a la lista correspondiente
+      agrupadoPorFecha[index ?? 0].data.push(paquete);
     }
-
-    // Añadir el paquete a la lista correspondiente
-    agrupadoPorFecha[fecha].push(paquete);
   });
 
   return agrupadoPorFecha;
