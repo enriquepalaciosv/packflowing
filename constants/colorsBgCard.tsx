91856ff378234d89c72694fc9a8968b6f36f5904
Lisandro <salvareschilisandro0@gmail.com>
2025-04-01 18:20:32 +0000

    change colors in cards

--- constants/colorsBgCard.tsx
+++ constants/colorsBgCard.tsx
@@ -1,11 +1,4 @@
 const COLORS = {
-  "Paquetes recibidos": ["#6FAED9", "#91C8E7"], 
-  "Paquetes en tránsito": ["#F4C842", "#FFD166"],
-  "Paquetes listos para recoger": ["#62B674", "#81C995"],
-  "Paquetes entregados": ["#E58AA5", "#F4A6C0"],
-};
-
-const COLORS_REFERENCES = {
   "Paquetes recibidos": ["#0072FF", "#00C6FF"],
   "Paquetes en tránsito": ["#F7B733", "#FC4A1A"],
   "Paquetes listos para recoger": ["#56ab2f", "#a8e063"],
