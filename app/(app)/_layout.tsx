b0f1e6d918e410575759611c7bd329ce90d345d1
Lisandro <salvareschilisandro0@gmail.com>
2025-04-08 14:17:34 +0000

    delete change-password route

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Redirect, Stack } from "expo-router";
 import { Text } from "react-native";
 import { useSession } from "../../contexts/authentication";
+import { PaperProvider } from 'react-native-paper';
 
 export default function AppLayout() {
   const { session, isLoading } = useSession();
@@ -8,33 +9,28 @@ export default function AppLayout() {
   if (isLoading) return <Text>Loading...</Text>;
   if (!session) return <Redirect href="/sign-in" />;
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false, title: "Inicio" }}
-      />
+    <PaperProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false, title: "Inicio" }}
+        />
 
-      <Stack.Screen
-        name="listAllPackages"
-        options={{ headerShown: true, title: "" }}
-      />
+        <Stack.Screen
+          name="listAllPackages"
+          options={{ headerShown: true, title: "" }}
+        />
 
-      <Stack.Screen
-        name="detailPackage"
-        options={{
-          headerShown: true,
-          title: "Detalles del Paquete",
-          headerTintColor: "#0f0f0f",
-          headerBackButtonDisplayMode: "minimal",
-        }}
-      />
-
-      <Stack.Screen
-        name="change-password"
-        options={{
-          presentation: "formSheet",
-        }}
-      />
-    </Stack>
+        <Stack.Screen
+          name="detailPackage"
+          options={{
+            headerShown: true,
+            title: "Detalles del Paquete",
+            headerTintColor: "#0f0f0f",
+            headerBackButtonDisplayMode: "minimal",
+          }}
+        />
+      </Stack>
+    </PaperProvider>
   );
 }
