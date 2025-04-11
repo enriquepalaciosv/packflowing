fab07207a781fe0d7e908a0cd5976524b4db1824
Lisandro <salvareschilisandro0@gmail.com>
2025-04-11 15:58:08 +0000

    add message when empty list and padding in list

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -37,70 +37,98 @@ export default function Index() {
     );
   }
 
+  const allEmpty = packages.every((item) => item.data.length === 0);
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <HeaderPackages name={session.name} lockerCode={session.lockerCode} />
-      <RefreshAnimation>
-        <SectionList
-          keyExtractor={(_, index) => index.toString()}
-          sections={packages.map((section) => ({
-            ...section,
-            total: section.data.length, // Número total de paquetes
-            totalData: section.data, // Listado completo de paquetes
-            data: section.data.slice(0, 5), // Solo los cinco más recientes
-          }))}
-          ListHeaderComponent={null}
-          ListEmptyComponent={() => <></>}
-          renderSectionHeader={({ section }) =>
-            section.total ? (
-              <View
-                style={{
-                  position: "relative",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text variant="titleMedium" style={{ marginVertical: 10 }}>
-                  {`${section.title} (${section.total})`}
-                </Text>
-                {section.total > 5 ? (
-                  <TouchableOpacity
-                    onPress={() =>
-                      router.push(
-                        `/(app)/listAllPackages?section=${section.path}`
-                      )
-                    }
-                  >
-                    <Text>Ver todos</Text>
-                  </TouchableOpacity>
-                ) : null}
-              </View>
-            ) : null
-          }
-          stickySectionHeadersEnabled={false}
-          stickyHeaderHiddenOnScroll={false}
-          renderItem={({ item, section }) => (
-            <PackageItem
-              id={item.idRastreo}
-              section={section.title}
-              via={item.via}
-              title={item.idRastreo}
-              name={session.name + " " + session.lastName}
-            />
-          )}
-          contentContainerStyle={styles.sectionList}
-          refreshing={isLoading}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
+      {
+        allEmpty ? (
+          <ListyEmpty />
+        ) : <ListPackages
+          isLoading={isLoading}
+          packages={packages}
+          session={session}
+          refreshing={refreshing}
+          onRefresh={onRefresh}
         />
-      </RefreshAnimation>
-    </SafeAreaView>
+      }
+    </SafeAreaView >
   );
-}
+};
+
+const ListPackages = ({ isLoading, packages, session, refreshing, onRefresh }) => {
+  const router = useRouter();
+
+  return (
+    <RefreshAnimation>
+      <SectionList
+        keyExtractor={(_, index) => index.toString()}
+        sections={packages.map((section) => ({
+          ...section,
+          total: section.data.length, // Número total de paquetes
+          totalData: section.data, // Listado completo de paquetes
+          data: section.data.slice(0, 5), // Solo los cinco más recientes
+        }))}
+        ListHeaderComponent={null}
+        ListEmptyComponent={() => <></>}
+        renderSectionHeader={({ section }) =>
+          section.total ? (
+            <View
+              style={{
+                position: "relative",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text variant="titleMedium" style={{ marginVertical: 10 }}>
+                {`${section.title} (${section.total})`}
+              </Text>
+              {section.total > 5 ? (
+                <TouchableOpacity
+                  onPress={() =>
+                    router.push(
+                      `/(app)/listAllPackages?section=${section.path}`
+                    )
+                  }
+                >
+                  <Text>Ver todos</Text>
+                </TouchableOpacity>
+              ) : null}
+            </View>
+          ) : null
+        }
+        stickySectionHeadersEnabled={false}
+        stickyHeaderHiddenOnScroll={false}
+        renderItem={({ item, section }) => (
+          <PackageItem
+            id={item.idRastreo}
+            section={section.title}
+            via={item.via}
+            title={item.idRastreo}
+            name={session.name + " " + session.lastName}
+          />
+        )}
+        contentContainerStyle={styles.sectionList}
+        refreshing={isLoading}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </RefreshAnimation>
+  )
+};
+
+const ListyEmpty = () => (
+  <View style={{ flex: 1, justifyContent: "flex-start", alignItems: "center", marginTop: 50 }}>
+    <Text variant="titleMedium" style={{ fontWeight: 400 }}>
+      No hay información disponible aún
+    </Text>
+  </View>
+)
 
 const styles = StyleSheet.create({
   safeAreaView: { flex: 1, backgroundColor: "#f0f0f0" },
-  sectionList: { paddingHorizontal: 20, paddingTop: 20, paddingBottom: 170 },
+  sectionList: { paddingHorizontal: 20, paddingTop: 20, paddingBottom: Platform.OS === "android" ? 200 : 170 },
 });
