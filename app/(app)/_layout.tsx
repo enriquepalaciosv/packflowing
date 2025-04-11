e88ea6324eab6b84a50d461ebec0610654463545
Lisandro <salvareschilisandro0@gmail.com>
2025-04-11 15:57:29 +0000

    configuration light theme by default

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,15 +1,14 @@
 import { Redirect, Stack } from "expo-router";
 import { Text } from "react-native";
 import { useSession } from "../../contexts/authentication";
-import { PaperProvider } from 'react-native-paper';
-
+import { MD3LightTheme, PaperProvider } from 'react-native-paper';
 export default function AppLayout() {
   const { session, isLoading } = useSession();
 
   if (isLoading) return <Text>Loading...</Text>;
   if (!session) return <Redirect href="/sign-in" />;
   return (
-    <PaperProvider>
+    <PaperProvider theme={MD3LightTheme}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen
           name="(tabs)"
