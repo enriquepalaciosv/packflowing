c45b12f44da2f622957cdbd483627333321f5b25
Slisandro <salvareschilisandro0@gmail.com>
2025-04-14 19:21:38 +0000

    add custom message when agencia is not active

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,20 +1,31 @@
 import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { Platform, View } from "react-native";
+import { Platform, Text, View } from "react-native";
 import ButtonChatIa from "../../../components/ButtonChatIa";
 import { useEffect, useState } from "react";
 import { getAgenciaByDefault } from "../../../firebase/agencia/services";
 
 export default function TabsLayout() {
   const [ai, setAI] = useState(null);
+  const [activo, setActivo] = useState(null);
 
   useEffect(() => {
     getAgenciaByDefault()
-      .then((response: { AI: boolean }) => setAI(response.AI))
+      .then((response: { AI: boolean; activo: boolean }) => {
+        setAI(response.AI);
+        setActivo(response.activo);
+      })
       .catch((error) => console.log({ error }));
   }, []);
 
   if (ai === null) return null;
+
+  if (!activo)
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Servicio no disponible por el momento</Text>
+      </View>
+    );
 
   return (
     <>
