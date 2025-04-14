6d33c5ebee4cb5d3c1b3a6eec1650d870d9d7f32
Slisandro <salvareschilisandro0@gmail.com>
2025-04-14 13:48:59 +0000

    desactive of font scaling in button ia

--- components/ButtonChatIa.tsx
+++ components/ButtonChatIa.tsx
@@ -1,46 +1,48 @@
 import { Feather } from "@expo/vector-icons";
 import { useEffect, useState } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import { Text } from "react-native-paper";
+import { StyleSheet, TouchableOpacity, Text } from "react-native";
 import ChatIaModal from "./ChatIaModal";
 import { getAgenciaByDefault } from "../firebase/agencia/services";
 
 export default function ButtonChatIa() {
-    const [visible, setVisible] = useState(false);
-    const [ai, setAI] = useState(null);
-    const handleModal = () => setVisible(!visible);
+  const [visible, setVisible] = useState(false);
+  const [ai, setAI] = useState(null);
+  const handleModal = () => setVisible(!visible);
 
-    useEffect(() => {
-        getAgenciaByDefault()
-            .then((response: { AI: boolean }) => setAI(response.AI))
-            .catch(error => console.log({ error }))
-    }, [])
+  useEffect(() => {
+    getAgenciaByDefault()
+      .then((response: { AI: boolean }) => setAI(response.AI))
+      .catch((error) => console.log({ error }));
+  }, []);
 
+  if (ai === null) return null;
 
-    if(ai === null) return null;
-
-    return (
-        <>
-            <ChatIaModal visible={visible} hideModal={handleModal} />
-            <TouchableOpacity style={styles.container} onPress={handleModal}>
-                <Feather size={28} name="help-circle" color={"#8f8f8f"} />
-                <Text variant="labelMedium" style={{ color: "#8f8f8f" }}>Buscar</Text>
-            </TouchableOpacity>
-        </>
-    )
+  return (
+    <>
+      <ChatIaModal visible={visible} hideModal={handleModal} />
+      <TouchableOpacity style={styles.container} onPress={handleModal}>
+        <Feather size={28} name="help-circle" color={"#8E8E8F"} />
+        <Text
+          allowFontScaling={false}
+          style={{ color: "#8E8E8F", fontSize: 12, fontWeight: 500 }}
+        >
+          Buscar
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        position: "absolute",
-        right: "50%",
-        bottom: 0,
-        transform: [{ translateX: 30 }],
-        alignItems: "center",
-        justifyContent: "center",
-        height: 60,
-        width: 60,
-        paddingTop: 5,
-        gap: 2
-    },
+  container: {
+    position: "absolute",
+    bottom: 60 / 2 - 30,
+    left: "50%",
+    transform: [{ translateX: -30 }],
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    width: 60,
+    gap: 4,
+  },
 });
